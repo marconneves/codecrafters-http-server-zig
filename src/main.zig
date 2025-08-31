a43a128559a88857964da5c668219cf1fa13ac17
Marcon Neves <marcon@codgital.com>
2025-08-31 17:03:52 +0000

    feat: atualizar amain base

--- src/main.zig
+++ src/main.zig
@@ -16,6 +16,22 @@ pub fn main() !void {
     var connection = try listener.accept();
     try stdout.print("client connected!", .{});
 
-    _ = try connection.stream.writer().write("HTTP/1.1 200 OK\r\n\r\n");
+    var buffer: [1024]u8 = undefined;
+
+    const bytes_read = try connection.stream.reader().read(&buffer);
+
+    if (bytes_read > 0) {
+        const request_data = buffer[0..bytes_read];
+        try stdout.print("Buffer como string: {s}\n", .{request_data});
+
+        if (std.mem.indexOf(u8, request_data, "GET / ") != null) {
+            _ = try connection.stream.writer().write("HTTP/1.1 200 OK\r\n\r\n");
+        } else {
+            _ = try connection.stream.writer().write("HTTP/1.1 404 Not Found\r\n\r\n");
+        }
+    } else {
+        _ = try connection.stream.writer().write("HTTP/1.1 500 Internal Server Error\r\n\r\n");
+    }
+
     connection.stream.close();
 }
